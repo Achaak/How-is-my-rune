3c2d75831aa08e42d4a408e11098015537c76f06
Achaak <achaak@users.noreply.github.com>
2018-06-22 09:29:51 +0200

    Update

--- controller/index.php
+++ controller/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <!--<base href="/">-->
+    <base href="/">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
     <title>How is my rune ? | SWRATE</title>
